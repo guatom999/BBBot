91975e18cb42726ca470e65bd76bc07ece926aef
guatom999 <tngs159@hotmail.com>
2026-06-02 17:04:07 +0700

    fix getConfig changed

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
-	"os"
 
 	"github.com/guatom999/BBBot/config"
 	"github.com/guatom999/BBBot/server"
@@ -15,13 +13,7 @@ func main() {
 
 	_ = ctx
 
-	cfg := config.GetConfig(func() string {
-		if len(os.Args) < 2 {
-			log.Fatal("Error: .env path is required")
-		}
-		log.Printf("choosen env is :%v", os.Args[1])
-		return os.Args[1]
-	}())
+	cfg := config.GetConfig()
 
 	server.NewDiscordServer(&cfg).Start()
 
